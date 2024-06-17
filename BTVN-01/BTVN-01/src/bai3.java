5592bdfef6b5b69d17d27818abba8fba36ef6bb9
Soblu99 <sonbep1234@gmail.com>
2024-06-17 10:38:41 +0700

    Update bai3.java

--- BTVN-01/BTVN-01/src/bai3.java
+++ BTVN-01/BTVN-01/src/bai3.java
@@ -1,9 +1,13 @@
 public class bai3 {
     public static void main(String[] args) {
+        int a = 9, b = 5, c =10, d = 15;
+        int x = Math.max(a,b);
+        int s = Math.max(x,c);
+
         System.out.println("a = 9" );
         System.out.println("b = 5" );
         System.out.println("c = 10" );
         System.out.println("d = 15");
-        System.out.println("So lon nhat = " + Math.max(15,10));
+        System.out.println("so lon nhat = " + Math.max(s,d));
     }
 }
