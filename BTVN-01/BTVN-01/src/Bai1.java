b6bc26a254cf27abcd20d9e38aab21621a75254b
Soblu99 <sonbep1234@gmail.com>
2024-06-17 11:27:18 +0700

    Update Bai1.java

--- BTVN-01/BTVN-01/src/Bai1.java
+++ BTVN-01/BTVN-01/src/Bai1.java
@@ -2,11 +2,11 @@ public class Bai1 {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        int sum = a + b;
+        int sum = (a + b)*2;
         int tich = a * b;
         System.out.println("Chiều rộng = " + a);
         System.out.println("Chiều dài = " + b);
-        System.out.println("chu vi = " + "a + " + "b = " + sum);
+        System.out.println("chu vi = " + "(a + " + "b) * 2 = " + sum);
         System.out.println("Diện tích = " + "a *" + " b =" + tich);
 
 
